8a8e462062e77c022e82f0f0b6be6000f39b9451
F1xa <Dubineckiy@bk.ru>
2021-11-07 04:16:07 +0300

    feat: added functions 'addTask', 'changeStatus', 'deleteTask'

---
+++ main.js
@@ -0,0 +1,94 @@
+// Сегодня напишем самый простой TODO лист без графического интерфейса.
+
+// Хранилищем будет объект, а имена задач - ключами. 
+
+// const list = {
+//  "create a task": "In Progress",
+//  "make a bed": "Done",
+//  "write a post": "To Do",
+// }
+
+// Функция changeStatus - будет менять статус задачи 
+
+// changeStatus("write a post", "Done")
+
+// Функция addTask - добавляет новую задачу
+
+// addTask('have a walk')
+
+// Функция deleteTask - удаляет задачу
+
+// deleteTask('have a walk')
+
+// Функция showList будет выводить весь список дел в виде 
+
+// Todo: 
+//  "create a task",
+//  "make a bed",
+// In Progress: 
+//  "write a post"
+// Done:
+//  -
+
+
+
+const list = {
+  "create a task": "In progress",
+  "make a bed": "done",
+  "write a post": "To Do",
+ };
+
+
+function changeStatus(task, status) {
+  for (let key in list) {
+    if (key === task) {
+      list[task] = status;
+      break;
+    }
+  }
+};
+
+changeStatus("make a bed", "To Do");
+
+
+
+ function addTask(task) {
+  return list[task] = null;
+ };
+
+ addTask("go to the gym");
+
+
+
+function deleteTask(task) {
+  for (let key in list) {
+    if (key === task) {
+      delete list[key];
+      break;
+    }
+  }
+};
+
+deleteTask("go to the gym");
+
+
+function showList() {
+  for (let key in list) {
+    switch (list[key]) {
+      case "To Do":
+        return `ToDo: ${key}`;
+      case "In Progress":
+        return `In Progress: ${key}`;
+      case "Done":
+        return `Done: -`;
+    }
+  }
+};
+
+
+
+
+ console.log(showList());
+
+
+
